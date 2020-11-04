d5bbb49b07b4291d474f6b54481b9bd7b9b65a08
Sylvia <sylviapap1@gmail.com>
2020-11-03 22:15:25 -0800

    Comments

--- speech.js
+++ speech.js
@@ -6,8 +6,8 @@ const textToSpeech = (string) => {
   let voice = new SpeechSynthesisUtterance(string);
   voice.text = string;
   voice.lang = "en-US";
-  voice.volume = 1; //0-1 interval
+  voice.volume = 1;
   voice.rate = 1;
-  voice.pitch = 1; //0-2 interval
+  voice.pitch = 2; // Can be 0, 1, or 2
   synth.speak(voice);
 }
