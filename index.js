9dc218026bedbc3575ec1f6572e016d237a3b435
Sylvia <sylviapap1@gmail.com>
2020-11-04 01:13:52 -0800

    Fix regex

--- index.js
+++ index.js
@@ -12,19 +12,26 @@ document.addEventListener("DOMContentLoaded", () => {
 function output(input) {
   let product;
 
-  // Remove chars except letters, space, & digits
-  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+  // Regex remove non word/space chars
+  // Trim trailing whitespce
+  // Remove digits - not sure if this is best
+  // But solves problem of entering something like 'hi1'
+
+  let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
   text = text
     .replace(/ a /g, " ")   // 'tell me a story' -> 'tell me story'
     .replace(/i feel /g, "")
     .replace(/whats/g, "what is")
     .replace(/please /g, "")
-    .replace(/ please/g, "");
+    .replace(/ please/g, "")
+    .replace(/r u/g, "are you");
 
   if (compare(prompts, replies, text)) { 
     // Search for exact match in `prompts`
     product = compare(prompts, replies, text);
-  } else if (text.match(/coronavirus/gi)) {
+  } else if (text.match(/thank/gi)) {
+    product = "You're welcome!"
+  } else if (text.match(/(corona|covid|virus)/gi)) {
     // If no match, check if message contains `coronavirus`
     product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
   } else {
@@ -63,17 +70,28 @@ function addChat(input, product) {
   let userDiv = document.createElement("div");
   userDiv.id = "user";
   userDiv.className = "user response";
-  userDiv.innerHTML = `<img src="user.png" height="30px"><span>${input}</span>`;
+  userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
   messagesContainer.appendChild(userDiv);
 
   let botDiv = document.createElement("div");
+  let botImg = document.createElement("img");
+  let botText = document.createElement("span");
   botDiv.id = "bot";
+  botImg.src = "bot-mini.png";
+  botImg.className = "avatar";
   botDiv.className = "bot response";
-  botDiv.innerHTML = `<span>${product}</span><img src="bot-mini.png" height="30px">`;
+  botText.innerText = "Typing...";
+  botDiv.appendChild(botText);
+  botDiv.appendChild(botImg);
   messagesContainer.appendChild(botDiv);
-
   // Keep messages at most recent
   messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
-  
-  textToSpeech(product);
+
+  // Fake delay to seem "real"
+  setTimeout(() => {
+    botText.innerText = `${product}`;
+    textToSpeech(product)
+  }, 2000
+  )
+
 }
